71bf1079f75aed6d65c989f225f3964b17bad20f
Alexandre Carbono <carbono@zapalago.com>
2018-11-06 09:30:09 -0200

    alteracao voltando regexp prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://financeiro-api.herokuapp.com',
 
-  tokenWhitelistedDomains: [ 'financeiro-api.herokuapp.com' ],
+  tokenWhitelistedDomains: [ new RegExp('financeiro-api.herokuapp.com') ],
   tokenBlacklistedRoutes: [ new RegExp('\/oauth\/token') ]
 };
